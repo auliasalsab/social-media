93b8bdad2fe65b4b6324b2a8de74c49e72100b08
aulia <asalsabila979@gmail.com>
2023-03-16 14:33:39 +0700

    modify model add profile controller

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,22 @@
 class Profile < ApplicationRecord
+    belongs_to :user
+    
+    validates :user_id, presence: true
+    validates :phone_number, presence: true
+    validates :dob, presence: true
+    validates :gender, presence: true
+    validates :bio, presence: true
+
+
+    def new_attributes {
+        id: self.id,
+        user: self.user,
+        phone_number: self.phone_number,
+        dob: self.dob,
+        gender: self.gender,
+        bio: self.bio,
+
+        created_at: self.created_at,
+    }
+    end
 end
